63a6529fc00b03a03ce0581ac20cc53f54267c62
Leonardo Natali <leonardo.natali@hotmail.com>
2021-08-07 12:03:41 -0300

    chore: add sprin boot base application

---
+++ src/main/java/com/leonardonatali/smartclockpunch/SmartClockPunchApplication.java
@@ -0,0 +1,13 @@
+package com.leonardonatali.smartclockpunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartClockPunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartClockPunchApplication.class, args);
+	}
+
+}
