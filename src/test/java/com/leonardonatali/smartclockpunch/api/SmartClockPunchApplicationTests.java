78a3eecaf41ca073b00d6c3ed7edce0e11c4424d
Leonardo Natali <leonardo.natali@hotmail.com>
2021-08-08 22:21:06 -0300

    refactor: restore application initializer files

---
+++ src/test/java/com/leonardonatali/smartclockpunch/api/SmartClockPunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardonatali.smartclockpunch.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartClockPunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
